7d0b901b371f164568996a19a9c2138a8509f0bf
ylvis-beep <ylvis8915@gmail.com>
2025-12-06 22:38:30 +0300

    Update render_build.sh

--- bot/render_build.sh
+++ bot/render_build.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+apt-get update
+apt-get install -y wget gnupg
+
+wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+pip install -r requirements.txt
 
